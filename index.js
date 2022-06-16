14e1c5a23650c8f4d54b7411a7eb1be40c514386
JOY <joy.kemunto@student.moringaschool.com>
2022-06-16 13:05:01 +0300

    commit tree 8b998419c0c7a9b2a968583515dee30e0f0ba2b5

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+// My code for this project
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+  
+  
+  function updateDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
